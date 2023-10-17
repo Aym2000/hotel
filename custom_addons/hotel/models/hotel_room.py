3ffe280dd6874681d3f257941edda8fa3957b8a6
Aym2000 <143732925+Aym2000@users.noreply.github.com>
2023-10-17 02:45:55 -0700

    Add files via upload

--- custom_addons/hotel/models/hotel_room.py
+++ custom_addons/hotel/models/hotel_room.py
@@ -5,7 +5,7 @@ class Hotel_Room(models.Model):
     _name = "hotel.room"
 
     
-    room_no = fields.Char(String = " Room No :" required = True)
+    room_no = fields.Char(String = " Room No :", required=True)
     category_id = fields.Many2one(string = "category",  comodel_name="hotel.catagory")
     price = fields.Float(string = "price ", )
     minmum_price = fields.Float(string = "minmum price ", )
